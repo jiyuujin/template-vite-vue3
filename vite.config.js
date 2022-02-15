6da903a69e6a9fb141a730b6f128466c183548d4
jiyuujin <jiyuujinunite@gmail.com>
2022-02-15 20:29:00 +0900

    update plugins, others
    - Update Vite plugin for Vue, TypeScript and Vite
    - Delete ESLint configuration
    - Delete Jest

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  outDir: 'out'
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+module.exports = defineConfig({
+  outDir: 'out',
+  plugins: [
+    vue()
+  ],
+  server: {
+    fs: {
+      strict: true
+    }
+  },
+  optimizeDeps: {
+    include: ['vue']
+  }
+})
