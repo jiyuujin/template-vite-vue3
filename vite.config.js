c3ee41b7b8492b1ffa6180df822bc8e82c99d4f8
jiyuujin <jiyuujinunite@gmail.com>
2022-02-16 08:13:12 +0900

    refactor: auto import

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 module.exports = defineConfig({
   outDir: 'out',
   plugins: [
+    AutoImport({
+      imports: ['vue'],
+      dts: 'src/auto-imports.d.ts',
+    }),
     vue()
   ],
   server: {
